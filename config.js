2d18e7454cc77748660f6bb926e1f7a9c7937554
kkostrikova <kostrikova0907@gmail.com>
2026-08-29 20:09:22 +0300

    Add WeStudy lecture course URL

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ window.PE_CONFIG = {
   theoryDurationMinutes: 60,
   practicalExamUrl: "practical.html",
   practicalDurationMinutes: 30,
+  lectureCourseUrl: "https://westudy.ua/en/PoleEducation/course/519be545-a825-4517-9f7d-a075b071b6e9",
   supabaseUrl: "https://mffjcqpyfcptwahpxeom.supabase.co",
   supabasePublishableKey: "sb_publishable_G69GdUkmDVVZcAVUhLT9Mw_pADiXZOs"
 };
