157aa171ae7b0a171d2f1c575062f8f02327c6c1
kkostrikova <kostrikova0907@gmail.com>
2026-08-29 14:57:41 +0300

    Configure Supabase client for Judge Academy auth

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ window.PE_CONFIG = {
   passScore: 80,
   reviewMode: true,
   finalExamOpen: false,
-  finalExamUrl: ""
+  finalExamUrl: "",
+  supabaseUrl: "https://mffjcqpyfcptwahpxeom.supabase.co",
+  supabasePublishableKey: "sb_publishable_G69GdUkmDVVZcAVUhLT9Mw_pADiXZOs"
 };
