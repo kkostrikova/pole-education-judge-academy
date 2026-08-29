594f0b22e4faf2e41cd5259ebfd186d1a82fcd83
kkostrikova <kostrikova0907@gmail.com>
2026-08-29 16:23:33 +0300

    Configure final theory exam settings

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ window.PE_CONFIG = {
   passScore: 80,
   reviewMode: true,
   finalExamOpen: false,
-  finalExamUrl: "",
+  finalExamUrl: "final-theory.html",
+  theoryPassScore: 60,
+  theoryDurationMinutes: 60,
   supabaseUrl: "https://mffjcqpyfcptwahpxeom.supabase.co",
   supabasePublishableKey: "sb_publishable_G69GdUkmDVVZcAVUhLT9Mw_pADiXZOs"
 };
