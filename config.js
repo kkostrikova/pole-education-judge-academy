34d2af2c14fc0015e7245e2ad8f7c4f6bdf6d010
kkostrikova <kostrikova0907@gmail.com>
2026-09-13 14:40:02 +0300

    Update WeStudy lecture course link

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.PE_CONFIG = {
   theoryDurationMinutes: 60,
   practicalExamUrl: "practical.html",
   practicalDurationMinutes: 30,
-  lectureCourseUrl: "https://westudy.ua/en/PoleEducation/course/519be545-a825-4517-9f7d-a075b071b6e9",
+  lectureCourseUrl: "https://westudy.ua/PoleEducation/course/04332efd-86fe-42a1-beb1-fa4ab6f703bc",
   supabaseUrl: "https://mffjcqpyfcptwahpxeom.supabase.co",
   supabasePublishableKey: "sb_publishable_G69GdUkmDVVZcAVUhLT9Mw_pADiXZOs"
 };
